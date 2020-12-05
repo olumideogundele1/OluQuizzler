bfaa7e4da6b61d195b94df2523c365b084181a1e
olumide30 <ogundeleolumuyiwaolumide@gmail.com>
2020-12-05 02:56:09 +0100

    QUiz App initial

---
+++ lib/Question.dart
@@ -0,0 +1,13 @@
+class Question{
+
+  String question;
+
+  bool answer;
+
+  Question(String q, bool a){
+    this.question = q;
+    this.answer = a;
+  }
+
+
+}
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,64 @@
+import 'Question.dart';
+
+class QuizBrain{
+
+  int _questionNumber = 0;
+
+  List<Question> _questionBank = [
+    Question("Is Liverpool Better Than Manchester United?", true),
+    Question("Basketball is better than Football", false),
+    Question("Oranges are red", false),
+    Question('You can lead a cow down stairs but not up stairs.', false),
+    Question('Approximately one quarter of human bones are in the feet.', true),
+    Question('A slug\'s blood is green.', true),
+    Question('Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', true),
+    Question('It is illegal to pee in the Ocean in Portugal.', true),
+    Question(
+        'No piece of square dry paper can be folded in half more than 7 times.',
+        false),
+    Question(
+        'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+        true),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+        'The total surface area of two human lungs is approximately 70 square metres.',
+        true),
+    Question('Google was originally called \"Backrub\".', true),
+    Question(
+        'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+        true),
+    Question(
+        'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+        true),
+  ];
+
+    String getQuestion(){
+      return _questionBank[_questionNumber].question;
+    }
+
+    bool getAnswer(){
+      return _questionBank[_questionNumber].answer;
+    }
+
+    bool getEndOfQuestion(){
+      if(_questionNumber >= (_questionBank.length - 1)){
+        return false;
+      }else
+        return true;
+    }
+
+    void resetQuestion(){
+      _questionNumber = 0;
+    }
+
+
+    void setNextQuestion(){
+      if(_questionNumber < (_questionBank.length -  1)){
+        _questionNumber++;
+      }
+    }
+
+
+}
